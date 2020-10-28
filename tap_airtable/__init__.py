6834faa111137f51f400f9c6fd96df4df2921190
chris <chris@net-island.de>
2020-10-28 10:12:44 +0100

    better error message for invalid tokens

--- tap_airtable/__init__.py
+++ tap_airtable/__init__.py
@@ -1,4 +1,5 @@
-from singer import utils
+import singer
+import logging
 from tap_airtable.services import Airtable
 
 
@@ -6,13 +7,18 @@ REQUIRED_CONFIG_KEYS = [
     'token',
 ]
 
-def main():
-    args = utils.parse_args(REQUIRED_CONFIG_KEYS)
 
-    if args.discover:
-        Airtable.run_discovery(args)
-    elif args.properties:
-        Airtable.run_sync(args.config, args.properties)
+def main():
+    args = singer.utils.parse_args(REQUIRED_CONFIG_KEYS)
+
+    try:
+        if args.discover:
+            Airtable.run_discovery(args)
+        elif args.properties:
+            Airtable.run_sync(args.config, args.properties)
+    except Exception as e:
+        singer.logger.log_error(str(e))
+        logging.exception(e)
 
 
 if __name__ == "__main__":
