ec53f1e620c0354f378dc1b175fa9f85875d2540
Salih Tuc <salihtuc@hotmail.com>
2024-02-08 12:16:03 +0300

    SRC-29: OAuth transformation (#22)
    * Updated metadata url
    
    * Removed urllib requirement since it breaks the container dependencies.

--- tap_airtable/services/__init__.py
+++ tap_airtable/services/__init__.py
@@ -32,7 +32,7 @@ class CatalogEntry(CatalogEntry):
 
 
 class Airtable(object):
-    metadata_url = "https://api.airtable.com/v2/meta/"
+    metadata_url = "https://api.airtable.com/v0/meta/bases/"
     records_url = "https://api.airtable.com/v0/"
     token = None
     selected_by_default = False
@@ -90,7 +90,7 @@ class Airtable(object):
     def discover_base(cls, base_id, base_name=None):
         cls.logger.info("discover base " + base_id)
         headers = cls.__get_auth_header()
-        response = cls.session.get(url=cls.metadata_url + base_id, headers=headers)
+        response = cls.session.get(url=cls.metadata_url + base_id + "/tables", headers=headers)
         response.raise_for_status()
         entries = []
 
@@ -126,7 +126,7 @@ class Airtable(object):
 
                 meta = metadata.write(meta, ('properties', field_name), 'inclusion', 'available')
                 meta = metadata.write(meta, ('properties', field_name), 'real_name', field['name'])
-                meta = metadata.write(meta, ('properties', field_name), 'airtable_type', field["config"]["type"] or None)
+                meta = metadata.write(meta, ('properties', field_name), 'airtable_type', field["type"] or None)
                 meta = metadata.write(meta, ('properties', field_name), 'airtable_field_ids', [field["id"]])
 
             schema = Schema(type='object', properties=schema_cols)
